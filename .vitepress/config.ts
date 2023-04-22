24bcd217ee85bb0b803f17cf983a4bf65d1f14fe
Madao <gn00678465@gmail.com>
2023-04-22 17:32:07 +0800

    build: update document config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -1,28 +1,29 @@
-import { defineConfig } from 'vitepress'
+import { defineConfig } from 'vitepress';
+import path from 'path';
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "tripplus-frontend-docs",
-  description: "tripplus frontend documents",
+  title: "tripplus-docs",
+  description: "frontend documents",
+  srcDir: `${path.resolve(process.cwd())}/src`,
   themeConfig: {
     // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
+      { text: 'Home', link: '/' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'Guide',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'Getting start', link: '/guide/' },
+          { text: 'System request', link: '/guide/request' }
         ]
       }
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/TripPlus-003/Frontend' }
     ]
   }
 })
